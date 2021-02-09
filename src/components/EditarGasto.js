29fc601ff641f76e202dbb5a63b0e6e0c0190638
Sisifo-G <manuelperdomo1710@icloud.com>
2021-02-09 17:43:20 -0500

    Componente editar gasto

--- src/components/EditarGasto.js
+++ src/components/EditarGasto.js
@@ -1,10 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { Helmet } from "react-helmet";
+import BtnRegresar from "../elements/BtnRegresar";
+import { Header, Titulo } from "./../elements/Header";
+import BarraTotalGastado from "./BarraTotalGastado";
+import FormularioGasto from "./FormularioGasto";
+import { useParams } from "react-router-dom";
+import useObtenerGasto from "./../hooks/useObtenerGasto";
 
 const EditarGasto = () => {
-    return ( 
-        <h1>Editar Gasto</h1>
-     );
-}
- 
+  const { id } = useParams();
+  const [gasto] = useObtenerGasto(id);
+
+  return (
+    <>
+      <Helmet>
+        <title>Editar Gasto</title>
+      </Helmet>
+      <Header>
+        <BtnRegresar ruta="/" />
+        <Titulo>Editar Gasto</Titulo>
+      </Header>
+      <FormularioGasto />
+      <BarraTotalGastado />
+    </>
+  );
+};
+
 export default EditarGasto;
